690aa507ca51eb1c05bc3b74cea0482ccbc0c168
Jeremy Fleming <thevisionmachine@gmail.com>
2024-05-09 07:04:28 -0500

    added prompt questions

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+const questions = require('./lib/questions');
+
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
