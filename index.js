7e94fa351b1cd7abbc3f7718adf896853f835863
Jeremy Fleming <thevisionmachine@gmail.com>
2024-05-09 08:30:49 -0500

    initialized svg-generator functionality

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const inquirer = require('inquirer');
 const questions = require('./lib/questions');
+const generateSvg = require('./lib/svg_generator');
+const fs = require('fs');
+
+console.log("Welcome to the Vector Graphics File Forge!");
+console.log("Please answer the following questions to generate your vector graphic file");
+console.log("-------------------------------");
 
 inquirer
   .prompt(questions)
   .then((answers) => {
     // Use user feedback for... whatever!!
+    fs.writeFile('./examples/example_svg.svg', generateSvg(answers), err => {
+      if (err) {
+        console.log('-----------------------');
+        console.log(err);
+    } else {
+        console.log('-----------------------');
+        console.log('File has been successfully written');
+    }
+    });
+
+    // console.log(answers);
   })
   .catch((error) => {
     if (error.isTtyError) {
