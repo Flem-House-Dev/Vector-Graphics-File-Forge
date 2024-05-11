4bea6de141700feb407bfc2433580e7129f1e438
Jeremy Fleming <thevisionmachine@gmail.com>
2024-05-11 12:23:27 -0500

    updated svg generator functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const questions = require('./lib/questions');
-const generateSvg = require('./lib/svg_generator');
+const createShapeObject = require('./lib/svg_generator');
 const fs = require('fs');
 
 console.log("Welcome to the Vector Graphics File Forge!");
@@ -11,22 +11,42 @@ inquirer
   .prompt(questions)
   .then((answers) => {
     // Use user feedback for... whatever!!
-    fs.writeFile('./examples/example_svg.svg', generateSvg(answers), err => {
-      if (err) {
-        console.log('-----------------------');
-        console.log(err);
-    } else {
-        console.log('-----------------------');
-        console.log('File has been successfully written');
-    }
-    });
+    console.log("prompt answers----------");
+    console.log(answers);
+    console.log("shape object----------");
+    console.log()
 
-    // console.log(answers);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+    try {
+      const shapeObject = createShapeObject(answers);
+      const svgString = shapeObject.getSVG();
+    
+      fs.writeFile("./examples/example_svg.svg", svgString, (err) => {
+        if (err) throw err;
+        console.log("SVG file created successfully!");
+      });
+    } catch (error) {
+      console.error(error.message);
     }
-  });
+
+  //   fs.writeFile('./examples/example_svg.svg', createShapeObject(answers), err => {
+  //     if (err) {
+  //       console.log('-----------------------');
+  //       console.log(err);
+  //       console.log("Didn't happen");
+  //   } else {
+  //       console.log('-----------------------');
+  //       console.log('File has been successfully written');
+  //   }
+  //   });
+
+  //   // console.log(answers);
+  // })
+  // .catch((error) => {
+  //   if (error.isTtyError) {
+  //     // Prompt couldn't be rendered in the current environment
+  //   } else {
+  //     // Something else went wrong
+  //   }
+  }
+);
+
